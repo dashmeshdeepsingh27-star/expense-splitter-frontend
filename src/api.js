ade21d851eac30debbbb6a1e6d2a96eb5c6e240d
Dashmesh <dashmeshdeepsingh27@gmail.com>
2026-08-16 11:59:18 +0530

    Remove stray / in base url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-splitter-backend-q8p0.onrender.com/api";\
+export const BASE_URL = "https://expense-splitter-backend-q8p0.onrender.com/api";
 
 
 function authHeaders() {
